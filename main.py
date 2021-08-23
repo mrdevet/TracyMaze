fa0fa5ad58360fad2b6585422980c87795923236
Casey Devet <casey.devet@sccdsb.net>
2021-08-23 12:35:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# This command allows us to use the functions 
+# provided by the Python turtle module
+from turtle import *
+
+# These commands set up the screen and config 
+# the "turtle"
+setup(480, 360)
+shape("turtle")
+color("green")
+pensize(5)
+
+# This command loads the maze.  If you finish 
+# one and want to try another, change the 
+# filename in quotes to be any of the .gif 
+# files listed in the Files panel
+bgpic("easy1.gif")
+
+# INSTRUCTIONS
+# Use the forward(), left() and right() 
+# functions to move Tracy through the maze to 
+# the exit without going through the walls!
+
